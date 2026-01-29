ee332f5f966ca37aa1eef5cab30cd09a2dd7e1f9
Matteo AUGER <33668505+matteoauger@users.noreply.github.com>
2026-01-29 09:02:27 +0100

    Update wezterm.lua
    Add kitty keyboard
    Add Ctrl+Enter keybind for fullscreen
    Add hide tab bar if only one tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,18 +4,24 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
+-- Visual
 config.initial_cols = 120
 config.initial_rows = 28
 
 config.font_size = 12
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
---config.font = wezterm.font 'BigBlueTermPlus Nerd Font'
 config.color_scheme = 'Tomorrow Night Burns'
+config.hide_tab_bar_if_only_one_tab = true
 
--- QoL
+-- Behavior
 config.window_close_confirmation = 'NeverPrompt'
+
+-- Keybinds
+config.keys = {
+    -- Fullscreen toggle
+    { key = 'Enter', mods = 'CTRL', action = wezterm.action.ToggleFullScreen, }
+}
+config.enable_kitty_keyboard = true
 
 -- Finally, return the configuration to wezterm:
 return config
